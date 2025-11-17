3eaf3f11de7ce11b925ffe9762499042d62c7c74
pvhk <p.vanhecke86@gmail.com>
2025-11-17 21:48:11 +0100

    test config api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   adapter: vercel(),
   site: 'https://www.askingfranklin.com',
   base: '/',
